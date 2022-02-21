53f711cbc5c9fe91e399d491e286d684f99733fd
MagnificentRoast <94555704+MagnificentRoast@users.noreply.github.com>
2022-02-21 11:30:16 -0700

    code updates

--- app.js
+++ app.js
@@ -1,161 +1,161 @@
+// imports the fs module from node.js
+const fs = require('fs');
+// inports inquirer
 const inquirer = require('inquirer');
-// const fs = require('fs');
-// const generatePage = require('./src/page-template');
+// imports page-template.js
+const generatePage = require('./src/page-template');
 
+// prompts to get basic information
 const promptUser = () => {
-    //Asks for basic user information
-    return inquirer.prompt([
-        {
-            type: 'input',
-            name: 'name',
-            message: 'What is your name? (Required)',
-            // validate function makes sure that you enter a valid name instead of leaving it blank. Validate evaluates the boolean 'true'
-            validate: nameInput => {
-                if (nameInput) {
-                    return true;
-                } else {
-                    console.log('Please enter your name!');
-                    return false;
-                }
-            }
-        },
-        {
-            type: 'input',
-            name: 'github',
-            message: 'Enter your GitHub Username (Required)',
-            validate: githubInput => {
-                if (githubInput) {
-                    return true;
-                } else {
-                    console.log('Please enter your GitHub user-name!');
-                }
-            }
-        },
-        {
-            type: 'confirm',
-            name: 'confirmAbout',
-            message: 'Would you like to enter some information about yourself for an about section?',
-            default: true
-        },
-        {
-            type: 'confirm',
-            name: 'confirmAbout',
-            message: 'Would you like to enter some information about yourself for an "about" section?',
-            default: true
-        },
-        {
-            type: 'input',
-            name: 'about',
-            message: 'Provide some information about yourself:',
-            when: ({ confirmAbout }) => {
-                if (confirmAbout) {
-                    return true;
-                } else {
-                    return false;
-                }
-            }
+  return inquirer.prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name? (Required)',
+      // validate function ensures that users will answer this question
+      validate: nameInput => {
+        if (nameInput) {
+          return true;
+          // error if field is blank
+        } else {
+          console.log('Please enter your name!');
+          return false;
         }
-    ]);
-};
-
-// To ask about projects from the user
-const promptProject = portfolioData => {
-    portfolioData.projects = [];
-    // if there's no 'projects' array property, create one
-    if (!portfolioData.projects) {
-        portfolioData.projects = [];
+      }
+    },
+    {
+      type: 'input',
+      name: 'github',
+      message: 'Enter your GitHub Username (Required)',
+      // validates the github field having content
+      validate: githubInput => {
+        if (githubInput) {
+          return true;
+          // error if field is blank
+        } else {
+          console.log('Please enter your GitHub username!');
+          return false;
+        }
+      }
+    },
+    {
+      type: 'confirm',
+      name: 'confirmAbout',
+      message: 'Would you like to enter some information about yourself for an "About" section?',
+      default: true
+    },
+    {
+      type: 'input',
+      name: 'about',
+      message: 'Provide some information about yourself:',
+      when: ({ confirmAbout }) => confirmAbout
     }
-    console.log(`
-    =================
-    Add a New Project
-    =================
-    `);
-    return inquirer.prompt([
-        {
-            type: 'input',
-            name: 'name',
-            message: 'What is the name of your project? (Required)',
-            validate: projectNameInput => {
-                if (projectNameInput) {
-                    return true;
-                } else {
-                    console.log('Please enter the name of your github project');
-                };
-            }
-        },
-        {
-            type: 'input',
-            name: 'description',
-            message: 'What is the description of your project? (Required)',
-            validate: descriptionInput => {
-                if (descriptionInput) {
-                    return true;
-                } else {
-                    console.log('Please enter the description of your project!');
-                }
-            }
-        },
-        {
-            type: 'checkbox',
-            name: 'languages',
-            messaage: 'What did you build this project with? (Check all that apply)',
-            choices: ['JavaScript', 'HTML', 'CSS', 'ES6', 'jQuery', 'bootstrap', 'node']
-        },
-        {
-            type: 'input',
-            name: 'link',
-            message: 'Enter the GitHub link to your project. (Required)',
-            validate: linkInput => {
-                if (linkInput) {
-                    return true;
-                } else {
-                    console.log('Please enter the link to your GitHub Project!')
-                }
-            }
-        },
-        {
-            type: 'confirm',
-            name: 'feature',
-            message: 'Would you like to feature this project?',
-            default: false
-        },
-        {
-            type: 'confirm',
-            name: 'confirmProject',
-            message: 'Would you like to enter another project?',
-            default: false
-        }
-        // .then(projectData => {
-        //     portfolioData.projects.push(projectData);
-        //     // This condition evaluates the user response to whether they wish to add more projects, the portfolioData was passed as an argument to prevent array overwrite.
-        //     if (projectData.confirmAddProject) {
-        //         return promptProject(portfolioData);
-        //     // If user selects false, it will not ask for further input and just show the one featured project instead
-        //     } else {
-        //         return portfolioData;
-        //     }
-        // })
-        // .then(projectData => {
-        //     portfolioData.projects.push(projectData);
-        //     if (projectData.confirmAddProject) {
-        //         return promptProject(portfolioData);
-        //     } else {
-        //         return portfolioData;
-        //     }
-        // })
-    ]);
+  ]);
 };
-//calls the function and shows the console log of your answer
-promptUser()
-    .then(promptProject)
-    .then(portfolioData => {
-        console.log(portfolioData);
+
+// additional information function
+const promptProject = portfolioData => {
+  console.log(`
+=================
+Add a New Project
+=================
+`);
+
+  // If there's no 'projects' array property, create one
+  if (!portfolioData.projects) {
+    portfolioData.projects = [];
+  }
+  return inquirer
+    .prompt([
+      {
+        type: 'input',
+        name: 'name',
+        message: 'What is the name of your project? (Required)',
+        validate: nameInput => {
+          if (nameInput) {
+            return true;
+          } else {
+            console.log('You need to enter a project name!');
+            return false;
+          }
+        }
+      },
+      {
+        type: 'input',
+        name: 'description',
+        message: 'Provide a description of the project (Required)',
+        validate: descriptionInput => {
+          if (descriptionInput) {
+            return true;
+          } else {
+            console.log('You need to enter a project description!');
+            return false;
+          }
+        }
+      },
+      {
+        type: 'checkbox',
+        name: 'languages',
+        message: 'What did you this project with? (Check all that apply)',
+        choices: ['JavaScript', 'HTML', 'CSS', 'ES6', 'jQuery', 'Bootstrap', 'Node']
+      },
+      {
+        type: 'input',
+        name: 'link',
+        message: 'Enter the GitHub link to your project. (Required)',
+        validate: linkInput => {
+          if (linkInput) {
+            return true;
+          } else {
+            console.log('You need to enter a project GitHub link!');
+            return false;
+          }
+        }
+      },
+      {
+        type: 'confirm',
+        name: 'feature',
+        message: 'Would you like to feature this project?',
+        default: false
+      },
+      {
+        type: 'confirm',
+        name: 'confirmAddProject',
+        message: 'Would you like to enter another project?',
+        default: false
+      }
+    ])
+    .then(projectData => {
+      portfolioData.projects.push(projectData);
+      if (projectData.confirmAddProject) {
+        return promptProject(portfolioData);
+      } else {
+        return portfolioData;
+      }
     });
+};
 
-// const pageHTML = generatePage(name, github);
+// calls promptUser function
+promptUser()
+  // calls promptProject
+  .then(promptProject)
+  .then(portfolioData => {
+    const pageHTML = generatePage(portfolioData);
 
-// fs.writeFile('./index.html', pageHTML, err => {
-//     if (err) throw err;
+    fs.writeFile('./index.html', pageHTML, err => {
+      if (err) throw new Error(err);
 
-//     console.log('Portfolio Complete! Check out index.html to see the output!');
-// })
+      console.log("Page created! Check out index.html in this directory to see it!");
+    });
+  });
+  // calls the portfolio data
+  // .then(portfolioData => {
+
+  // const pageHTML = generatePage();
+
+  // fs.writefile('/.index.html', pageHTMl, err => {
+  //   if (err) throw new Error(err);
+
+  //   console.log('Page created! Check out index.html in this directory to see it!);
+  // })
+  // });
--- src/page-template.js
+++ src/page-template.js
@@ -1,23 +1,233 @@
-// This file is used to generate a file using this HTML template. name and github are taken from the command line...
+// create the about section
+const generateAbout = aboutText => {
+  if (!aboutText) {
+    return '';
+  }
 
-const generatePage = (name, github) => {
-    return `
-    <!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta http-equiv="X-UA-Compatible" content="ie-edge">
-        <title>Portfolio Generator</title>
-    </head>
-    
-    <body>
-        <h1>${name}</h1>
-        <h2><a href="https://github.com/${github}">Github</a></h2>
-    </body>
-    </html>
+  return `
+    <section class="my-3" id="about">
+      <h2 class="text-dark bg-primary p-2 display-inline-block">About Me</h2>
+      <p>${aboutText}</p>
+    </section
     `;
 };
 
-// ...and this command exports the content of the page template to app.js
-module.exports = generatePage;
+// look at previous versions I'm not typing it 3 times in one hour. added map and join i guess.
+const generateProjects = projectsArr => {
+  return `
+    <section class="my-3" id="portfolio">
+      <h2 class="text-dark bg-primary p-2 display-inline-block">Work</h2>
+      <div class="flex-row justify-space-between">
+      ${projectsArr
+        .filter(({ feature }) => feature)
+        .map(({ name, description, languages, link }) => {
+          return `
+          <div class="col-12 mb-2 bg-dark text-light p-3">
+            <h3 class="portfolio-item-title text-light">${name}</h3>
+            <h5 class="portfolio-languages">
+              Built With:
+              ${languages.join(', ')}
+            </h5>
+            <p>${description}</p>
+            <a href="${link}" class="btn"><i class="fab fa-github mr-2"></i>View Project on GitHub</a>
+          </div>
+        `;
+        })
+        .join('')}
+
+      ${projectsArr
+        .filter(({ feature }) => !feature)
+        .map(({ name, description, languages, link }) => {
+          return `
+          <div class="col-12 col-md-6 mb-2 bg-dark text-light p-3 flex-column">
+            <h3 class="portfolio-item-title text-light">${name}</h3>
+            <h5 class="portfolio-languages">
+              Built With:
+              ${languages.join(', ')}
+            </h5>
+            <p>${description}</p>
+            <a href="${link}" class="btn mt-auto"><i class="fab fa-github mr-2"></i>View Project on GitHub</a>
+          </div>
+        `;
+        })
+        .join('')}
+      </div>
+    </section>
+  `;
+};
+
+// // generate the projects array for a section
+// const generateProjects = projectsArr => {
+//   // get array of just featured projects
+//   const featuredProjects = projectsArr.filter(project => {
+//     if (project.feature) {
+//       return true;
+//     } else {
+//       return false;
+//     }
+//   });
+
+//   // get array of all non-featured projects
+//   const nonFeaturedProjects = projectsArr.filter(project => {
+//     if (!project.feature) {
+//       return true;
+//     } else {
+//       return false;
+//     }
+//   });
+
+//   const featuredProjectHtmlArr = featuredProjects.map(({ name, description, languages, link }) => {
+//     return `
+//       <div class=col-12 mb-2 bg-dark text-light p-3 flex-column">
+//         <h3 class="portfolio item-title text-light p-3 flex-column">
+//         <h5 class="portfolio-languages">
+//           Built With:
+//           ${languages.join(', ')}
+//         </h5>
+//         <p>${description}</p>
+//         <a href="${link}" class="btn mt-auto"><i class="fab fa-github mr-2"></i>View Project on GitHub</a>
+//       </div>
+//     `;
+//   });
+
+//   const nonFeaturedProjectHtmlArr = nonFeaturedProjects.map(
+//     ({ name, description, languages, link }) => {
+//       return `
+//         <div class="col-12 col-md-6 mb-2 bg-dark text-light p-3 flex-column">
+//           <h3 class="portfolio item-title text-light">${name}</h3>
+//           <h5 class="portfolio-languages">
+//             Built With:
+//             ${languages.join(', ')}
+//           </h5>
+//           <p>${description}</p>
+//           <a href="${link}" class="btn mt-auto"><i class="fab fa-github mr-2"></i>View Project on GitHub</a>
+//         </div>
+//       `;
+//     }
+//   );
+
+//     return `
+//     <section class="my-3" id="portfolio">
+//       <h2 class="text-dark bg-primary p-2 display-inline-block">Work</h2>
+//       <div class="flex-row justify-space-between">
+//       ${featuredProjectHtmlArr.join('')}
+//       ${nonFeaturedProjectHtmlArr.join('')}
+//       </div>
+//     </section>
+//     `;
+// };
+
+// generate projects array and makes section for projects
+// const generateProjects = projectsArr => {
+//   const projectHtmlArr = projectsArr.map(({ name, description, languages, link }) => {
+//     return `
+//       <div class="col-12 col-md-6 mb-2 bg-dark text-light p-3 flex-column">
+//         <!-- Shows the name of the project -->
+//         <h3 class="portfolio-item-title text-light">${name}</h3>
+//         <h5 class="portfolio-languages">
+//         <!-- Shows what languages the project was built with -->
+//           Built With:
+//           ${languages.join(', ')}
+//         </h5>
+//         <!-- Shows the description of the project -->
+//         <p>${description}</p>
+//         <!-- generates a link in the form of a button that will take you to the github project link -->
+//         <a href="${link}" class="btn mt-auto"><i class="fab fa-github mr-2"></i>View Project on GitHub</a>
+//       </div>
+//     `;
+//   });
+
+//   return `
+//     <section class="my-3" id="portfolio">
+//       <h2> class="text-dark bg-primary p-2 display-inline-block">Work</h2>
+//       <div class="flex-row justify-space-between">
+//       ${projectHtmlArr.join('')}
+//       </div>
+//     </section>
+//   `;
+// };
+
+// projects array creation, so when you put your prject information in, it will make a new section.
+// const generateProjects = projectsArr => {
+//   return `
+//     <section class="my-3" id="portfolio">
+//       <h2 class="text-dark bg-primary p-2 display-inline-block">Work</h2>
+//       <div class="flex-row justify-space-between">
+//       <!-- Leaving this empty as we'll dynamically insert project HTML here -->
+//       </div>
+//     </section>
+//   `;
+// };
+
+module.exports = templateData => {
+  // destructure page data by section
+  const { projects, about, ...header } = templateData;
+
+  return `
+  <!DOCTYPE html>
+  <html lang="en">
+  
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Portfolio Demo</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css">
+    <link href="https://fonts.googleapis.com/css?family=Public+Sans:300i,300,500&display=swap" rel=stylesheet">
+    <link rel="stylesheet" href="style.css">
+  </head>
+
+  <body>
+    <header>
+      <div class="container flex-row justify-space-between align-center py-3">
+        <h1 class="page-title text-secondary bg-dark py-2 px-3">${header.name}</h1>
+        <nav class="flex-row">
+          <a class="ml-2 my-1 px-2 py-1 bg-secondary dext-dark" href="https://github.com/${
+            header.github
+          }">Github</a>
+        </nav>
+      </div>
+    </header>
+    <main class="container my-5">
+    <!-- Calls back about for this section -->
+      ${generateAbout(about)}
+    <!-- Calls back projects for this section -->
+      ${generateProjects(projects)}
+    </main>
+
+    <footer class="container text-center py-3">
+      <h3 class="text-dark">&copy; ${new Date().getFullYear()} by ${header.name}</h3>
+    </footer>
+  </body>
+  </html>
+    `;
+};
+
+// module.exports = templateData => {
+
+//   console.log(templateData);
+
+// // destructure projects and about data from templateData based on their property key names
+// const { projects, about, ...header } = templateData;
+//   return `
+//   <!DOCTYPE html>
+//   <html lang="en">
+
+//   <head>
+//     <meta charset="UTF-8">
+//     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+//     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+//     <title>Portfolio Demo</title>
+//   </head>
+
+//   <body>
+//     <h1>${templateData.name}</h1>
+//     <h2><a href="https://github.com/${templateData.github}">Github</a></h2>
+//   </body>
+//   </html>
+//   `;
+// };
+
+// .filter() filters out specific parts of an array and shows them to the user
+
+// .
